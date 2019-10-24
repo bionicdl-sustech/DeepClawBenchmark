63a7c9d6771c3157dff78d74d0b92d07e50b8a2a
Haiik <11510135@mail.sustech.edu.cn>
2019-10-24 10:58:55 +0800

    version 1.1

--- Driver/Cobotta/initial.sh
+++
@@ -1,4 +0,0 @@
-cd;
-cd deepclaw_ws;
-source devel/setup.bash;
-roslaunch denso_robot_bringup cobotta_bringup.launch sim:=false robot_address:=192.168.0.1;
--- Driver/Cobotta/server.sh
+++
@@ -1,4 +0,0 @@
-cd;
-cd deepclaw_ws;
-source devel/setup.bash;
-roslaunch bcap_service bcap_service.launch ip_address:=192.168.0.1;
