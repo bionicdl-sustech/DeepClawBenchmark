8d4693e46215eb231508e031b583af32e74c8c7c
Haiik <11510135@mail.sustech.edu.cn>
2020-03-30 15:46:00 +0800

    update release related

---
+++ deepclaw/modules/end2end/detectron2/projects/DensePose/densepose/utils/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2020 by BionicLab. All Rights Reserved.
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
+"""
+@File: __init__.py
+@Author: Haokun Wang
+@Date: 2020/3/30 15:42
+@Description: 
+"""
